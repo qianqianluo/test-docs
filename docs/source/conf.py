ec5bfd7cf5831e2b60bbc32eea7ac2327a84e3e1
Qian Luo <qianqianluo@gmail.com>
2021-07-31 01:32:38 -0700

    updated version and conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,9 @@
 #
 import os
 from pathlib import Path
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+#import sys
+#sys.path.insert(0, os.path.abspath('.'))
+
 
 
 # -- Project information -----------------------------------------------------
@@ -33,6 +34,10 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,36 +60,96 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
 
 
 locale_dirs = ['locale/']   # path is example but recommended.
 gettext_compact = False     # optional.
 
-
+import sys, os
+sys.path.insert(0, os.path.abspath('../source'))
 # multilangage support
+############################
+# SETUP THE RTD LOWER-LEFT #
+############################
 try:
-    html_context
-except:
-    html_context = dict()
-
+   html_context
+except NameError:
+   html_context = dict()
 html_context['display_lower_left'] = True
 
-# from`CURRENT_LANGUAGE`get parameter,default "en"
-current_language = os.environ.get('CURRENT_LANGUAGE') if os.environ.get('CURRENT_LANGUAGE') else 'en'
-html_context['current_language'] = current_language
-
-if current_language == 'zh_CN':
-    # # POPULATE LINKS TO OTHER LANGUAGES
-    html_context['languages'] = [('zh_CN', 'index.html')]
-
-    languages = [lang.name for lang in Path(__file__).parent.joinpath("locale").iterdir() if lang.is_dir()]
-    for lang in languages:
-        html_context['languages'].append((lang, f'{lang}/index.html'))
+if 'REPO_NAME' in os.environ:
+	REPO_NAME = os.environ['REPO_NAME']
 else:
-    # # POPULATE LINKS TO OTHER LANGUAGES
-    html_context['languages'] = [('zh_CN', '../index.html')]
-
-    languages = [lang.name for lang in Path(__file__).parent.joinpath("locale").iterdir() if lang.is_dir()]
-    for lang in languages:
-        html_context['languages'].append((lang, f'../{lang}/index.html'))
+	REPO_NAME = ''
+ 
+# SET CURRENT_LANGUAGE
+if 'current_language' in os.environ:
+   # get the current_language env var set by buildDocs.sh
+   current_language = os.environ['current_language']
+else:
+   # the user is probably doing `make html`
+   # set this build's current language to english
+   current_language = 'en'
+ 
+# tell the theme which language to we're currently building
+html_context['current_language'] = current_language
+ 
+# SET CURRENT_VERSION
+from git import Repo
+repo = Repo( search_parent_directories=True )
+ 
+if 'current_version' in os.environ:
+   # get the current_version env var set by buildDocs.sh
+   current_version = os.environ['current_version']
+else:
+   # the user is probably doing `make html`
+   # set this build's current version by looking at the branch
+   current_version = repo.active_branch.name
+ 
+# tell the theme which version we're currently on ('current_version' affects
+# the lower-left rtd menu and 'version' affects the logo-area version)
+html_context['current_version'] = current_version
+html_context['version'] = current_version
+ 
+# POPULATE LINKS TO OTHER LANGUAGES
+html_context['languages'] = [ ('en', '/' +REPO_NAME+ '/en/' +current_version+ '/') ]
+ 
+languages = [lang.name for lang in os.scandir('locales') if lang.is_dir()]
+for lang in languages:
+   html_context['languages'].append( (lang, '/' +REPO_NAME+ '/' +lang+ '/' +current_version+ '/') )
+ 
+# POPULATE LINKS TO OTHER VERSIONS
+html_context['versions'] = list()
+ 
+versions = [branch.name for branch in repo.branches]
+for version in versions:
+   html_context['versions'].append( (version, '/' +REPO_NAME+ '/'  +current_language+ '/' +version+ '/') )
+ 
+# POPULATE LINKS TO OTHER FORMATS/DOWNLOADS
+ 
+# settings for creating PDF with rinoh
+rinoh_documents = [(
+ master_doc,
+ 'target',
+ project+ ' Documentation',
+ '© ' +copyright,
+)]
+today_fmt = "%B %d, %Y"
+ 
+# settings for EPUB
+epub_basename = 'target'
+ 
+html_context['downloads'] = list()
+html_context['downloads'].append( ('pdf', '/' +REPO_NAME+ '/' +current_language+ '/' +current_version+ '/' +project+ '-docs_' +current_language+ '_' +current_version+ '.pdf') )
+ 
+html_context['downloads'].append( ('epub', '/' +REPO_NAME+ '/' +current_language+ '/' +current_version+ '/' +project+ '-docs_' +current_language+ '_' +current_version+ '.epub') )
+ 
+##########################
+# "EDIT ON GITHUB" LINKS #
+##########################
+ 
+html_context['display_github'] = True
+html_context['github_user'] = 'qianqianluo'
+html_context['github_repo'] = 'test-docs'
+html_context['github_version'] = 'master/docs/'
